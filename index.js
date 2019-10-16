e9b370ca8168034cb5a2523c5ce8226278e4678b
Misaruiz <misaruiz@gmail.com>
2019-10-15 23:00:37 -0700

    another update

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { DatePicker, message } from 'antd';
-import 'antd/dist/antd.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { version, Button } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 
-class App extends React.Component {
-  state = {
-    date: null,
-  };
-
-  handleChange = date => {
-    message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
-    this.setState({ date });
-  };
-
-  render() {
-    const { date } = this.state;
-    return (
-      <div style={{ width: 400, margin: '100px auto' }}>
-        <DatePicker onChange={this.handleChange} />
-        <div style={{ marginTop: 20 }}>
-          Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div className="App">
+    <h1>Please fork this codesandbox to reproduce your issue.</h1>
+    <div>Current antd version: {version}</div>
+    <div style={{ marginTop: "16px" }}>
+      <Button type="primary">Example button</Button>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
